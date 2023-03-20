d0fb12d65a1ef8035463516134e86e379897b0fd
SrNescau02 <102976772+SrNescau02@users.noreply.github.com>
2023-03-20 10:54:56 -0300

    Add files via upload

---
+++ lista 2-2.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a=11;
+ for(int i=1;i<=9;i++){
+     a+=10;
+     
+     
+         printf("\n %i",a);
+     }
+ }
+
