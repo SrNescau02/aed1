718a52aaa87615804e69035616d13451cc88dd3b
SrNescau02 <102976772+SrNescau02@users.noreply.github.com>
2023-03-20 11:48:17 -0300

    Add files via upload

---
+++ lista 4-9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int
+main ()
+{
+  float soma = 1;
+  int a;
+  int b;
+  printf ("\n digite quantos numeros quer digitar");
+  scanf ("\n %i", &a);
+
+  for (int i = 0; i < a; i++)
+    {
+
+      scanf ("\n %i", &b);
+      if (b % 5 == 0)
+	{
+	  soma = soma * b;
+	}
+    }
+  printf ("\n soma dos numeros =%f", soma);
+
+
+  return 0;
+}
