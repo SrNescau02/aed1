7a5922933b6ac1c066a14fffc070acdf95ef75bc
henrique <henriqueaom02@gmail.com>
2023-03-22 17:06:23 -0300

    Co-authored-by: Lucas Caldeira <Luqueze@users.noreply.github.com>

--- lista 4/12.c
+++ lista 4/12.c
@@ -12,21 +12,32 @@ int main()
 {
     int alunos;
     float nota;
-    float soma=0;
+    
     float media=0;
-    printf("digite quantos alunos quer testar");
+    
+
+    printf("digite quantos alunos quer testar \n ");
     scanf("\n %i",&alunos);
+
     for(int i =1;i<=alunos;i++){
+
+        float soma = 0;  
+    
         for(int a=1;a<=3;a++){
-             printf("digite a nota %i do aluno %i ",a, i);
+            printf("digite a nota %i do aluno %i \n ",a, i);
             scanf("\n %f",&nota);
             
             soma+=nota;
         }
+
+       
+
+        media=soma/alunos;
+        
+        printf("\n numero da soma = %f media = %f \n",soma,media);
         
     }
-    media=soma/alunos;
-    printf("\n numero da soma = %f media = %f",soma,media);
+
     
 
     return 0;
---
+++ lista 4/13.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+int numero;
+int primo;
+int contador=0;
+
+
+
+printf("\n digite um numero a ser testado");
+scanf("\n %i",&numero);
+
+
+for(int i=2;i<=numero/2;i++){
+
+    if(numero%i==0){
+
+        contador++;
+    }
+  
+
+}
+     
+    if(contador==0){
+
+        printf("\n numero primo");
+
+    }else
+
+        printf("\n numero nao primo");
+
+return 0;
+}
