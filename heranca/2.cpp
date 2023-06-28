fe507a96b484487458f5cb07b4bfe17ac4f1ce1e
SrNescau02 <102976772+SrNescau02@users.noreply.github.com>
2023-06-28 20:51:19 +0000

    klkkkkk

--- heranca/2.cpp
+++ heranca/2.cpp
@@ -22,6 +22,10 @@ class Retangulo : public Forma{
     float altura;
 
     public:
+    Retangulo(float base,float altura){
+        this -> base = base;
+        this -> altura = altura;
+    }
 
     double perimetro(){
        float perimetrofinal= base + base + altura +altura;
@@ -31,6 +35,14 @@ class Retangulo : public Forma{
     double area(){
         float areafinal = base*altura;
         return areafinal;
+    }
+
+    void imprimir(){
+        cout << "retangulo" << "base" << "altura" << endl;
+    }
+
+    void mostrarforma(){
+        cout << base << altura << endl;
     }
 
  
